504ace6038e570b6c214b6d1315b32eeb5cf2a66
Mahdi Shirani <shirani98@yahoo.com>
2023-02-04 13:21:49 +0330

    feat: add model methods

--- transaction/models/transaction.py
+++ transaction/models/transaction.py
@@ -1,18 +1,74 @@
 from django.db import models
-from transaction.models.courier import Courier
+from transaction.models import Courier
+from datetime import date
+from django.db.models import Sum, Q
+from django.db.models.functions import Coalesce
+from enum import Enum
 
-class Transaction(models.Model):
+class TransactionType(Enum):
+    """Enum class to represent the type of a Transaction."""
+
     TRIP = 1
     INCREASE = 2
     DECREASE = 3
 
-    TRANSACTION_TYPE_CHOICE = ((INCREASE, "Increase"), (DECREASE, "Decrease"), (TRIP, "Trip"))
-    
-    transaction_type = models.SmallIntegerField(choices=TRANSACTION_TYPE_CHOICE)
-    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
-    amount = models.PositiveIntegerField(default=0)
-    date = models.DateTimeField(auto_now_add=True)
-    description = models.JSONField(blank=True, null=True)
+
+class Transaction(models.Model):
+    """
+    Model to represent a Transaction.
+
+    A transaction represents a financial transaction between the company and a courier.
+    """
+
+    transaction_type = models.SmallIntegerField(
+        choices=[(type.value, type.name) for type in TransactionType],
+        verbose_name="Transaction Type",
+    )
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE,related_name='transactions')
+    amount = models.PositiveIntegerField(
+        default=0, verbose_name="Amount"
+    )
+    date = models.DateField(default=date.today, verbose_name="Date")
+    description = models.JSONField(blank=True, null=True, verbose_name="Description")
     
     def __str__(self):
-        return f"{self.courier.name} - {self.amount} - {self.get_transaction_type_display()}"
+        return f"{self.courier.name} - {self.amount} - {TransactionType(self.transaction_type).name}"
+
+    def _calculate_amount(self):
+        """Helper method to calculate the amount.
+
+        The amount is calculated by summing all positive transactions (TRIP and INCREASE) and
+        subtracting all negative transactions (DECREASE).
+        """
+        positive_amount = Sum(
+            "amount",
+            filter=Q(
+                transaction_type__in=[
+                    TransactionType.TRIP.value,
+                    TransactionType.INCREASE.value,
+                ]
+            ),
+        )
+        negative_amount = Sum(
+            "amount", filter=Q(transaction_type=TransactionType.DECREASE.value)
+        )
+
+        return self.courier.transactions.filter(
+            date=self.date
+        ).aggregate(
+            total_daily_amount=Coalesce(positive_amount, 0)
+            - Coalesce(negative_amount, 0)
+        )[
+            "total_daily_amount"
+        ]
+
+
+    def save(self, *args, **kwargs):
+        from financial_report.models import DailyReport
+
+        super().save(*args, **kwargs)
+        DailyReport.objects.update_or_create(
+            courier=self.courier,
+            date=self.date,
+            defaults={"amount": self._calculate_amount()},
+        )
